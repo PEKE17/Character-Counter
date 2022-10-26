0ea0a746457f7d100d301d61461d6747aaa6a3d0
PEKE17 <109898089+PEKE17@users.noreply.github.com>
2022-10-27 00:17:24 +0800

    Add files via upload

---
+++ character cunter/character counter.js
@@ -0,0 +1,10 @@
+const submitBtn = document.getElementById('submit-btn')
+const textArea = document.getElementById('text-area')
+const charCounter = document.getElementById('char-counter')
+
+
+submitBtn.addEventListener('click', function(){
+  let result = textArea.value.length
+  charCounter.innerHTML = 'Character Count:' + ' '+result
+  voweCounterFunction(textArea)
+})
